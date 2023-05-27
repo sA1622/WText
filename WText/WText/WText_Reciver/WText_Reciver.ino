1db0d1f8b25fbb231002104bb340fdf121db50a3
sA1622 <101582943+sA1622@users.noreply.github.com>
2023-05-27 17:54:56 +0300

    Update WText_Reciver.ino
    Changed the keyboard pins

--- WText/WText/WText_Reciver/WText_Reciver.ino
+++ WText/WText/WText_Reciver/WText_Reciver.ino
@@ -27,8 +27,8 @@ char keys[ROWS][COLS] = {
 };
 
 // connect the pins from right to left to pin 2, 3, 4, 5,6,7,8,9
-byte rowPins[ROWS] = {5,4,3,2}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {9,8,7,6}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {5,6,2,3}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {A0,A1,A2,A3}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
